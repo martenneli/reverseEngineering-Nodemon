7340ec99ea967121691dac2b49bbd2ebe613b637
Yassine <yassin23raki@gmail.com>
2020-10-13 11:30:13 +0100

    fixing the "add" event

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
 const chokidar = require('chokidar');
+const debounce = require('lodash.debounce');
+
+const start = debounce( () => {
+    console.log('STARTING USERS PROGRAM');
+}, 100)
 
 chokidar
     .watch('.')
-    .on('add', () => console.log('STARTING USERS PROGRAM'))
+    .on('add', start)
     .on('change', () => console.log('FILE CHANGED'))
     .on('unlink', () => console.log('FILE UNLINKED'))
-    
