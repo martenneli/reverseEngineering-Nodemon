86a18a20eaa96e2052ffb82024460a5ce135f2dc
Yassine <yassin23raki@gmail.com>
2020-10-14 07:30:26 +0100

    final v1.0

--- index.js
+++ index.js
@@ -2,13 +2,35 @@
 
 const chokidar = require('chokidar');
 const debounce = require('lodash.debounce');
+const program = require('caporal');
+const fs = require('fs');
+const { spawn } = require('child_process');
 
-const start = debounce( () => {
-    console.log('STARTING USERS PROGRAM');
-}, 100)
+program
+    .version('1.0.0')
+    .argument('[filename]', 'Name of a file to execute')
+    .action( async ({ filename }) => {
+        const name = filename || "index.js";
+        
+        try {
+            await fs.promises.access(name);    
+        } catch (err) {
+            throw new Error(`Could not find the file ${name}`)
+        }
 
-chokidar
-    .watch('.')
-    .on('add', start)
-    .on('change', () => console.log('FILE CHANGED'))
-    .on('unlink', () => console.log('FILE UNLINKED'))
+        let proc;
+        const start = debounce( () => {
+            if (proc) { proc.kill() };
+            console.log('>>>>>> Starting process...')
+            proc = spawn('node', [name], { stdio: 'inherit' });
+        }, 100)
+
+        chokidar
+            .watch('.')
+            .on('add', start)
+            .on('change', start)
+            .on('unlink', start)
+    }); 
+
+program.parse(process.argv)
+
